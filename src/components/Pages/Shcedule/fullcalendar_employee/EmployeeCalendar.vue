90dfe4bd1e77d935087fbd0630910994e66a5f0c
SerjNF <foreman@inbox.ru>
2020-03-10 09:46:15 +0700

    1

--- src/components/Pages/Shcedule/fullcalendar_employee/EmployeeCalendar.vue
+++ src/components/Pages/Shcedule/fullcalendar_employee/EmployeeCalendar.vue
@@ -21,8 +21,10 @@
           now-indicator="true"
           nav-links="true"
           selectable="true"
+          editable="true"
+          :custom-buttons="buttons"
           :header="{
-        left: 'prev,next today',
+        left: 'prev,next, today, interval',
         center: 'title',
         right: 'dayGridMonth,resourceTimeGridWeek,resourceTimeGridDay,listWeek'
       }"
@@ -30,12 +32,18 @@
           :selectable="true"
           :weekends="calendarWeekends"
           :events="events"
-
           @select="select"
+          @eventClick="eventClick"
+          @eventDrop="eventDrop"
+          @eventResize="eventResize"
         />
       </v-col>
       <v-col xl="2" lg="3" md="3" sm="12" class="pl-lg-0 pl-md-0 pa-sm-8 ">
-        <v-date-picker v-model="date" :show-current="false" width="auto"></v-date-picker>
+        <v-date-picker
+          v-model="date"
+          :show-current="true"
+          :picker-date.sync="pickerDate"
+          width="auto"></v-date-picker>
         <v-select
           v-model="employeeSelect"
           :items="employeesList"
@@ -47,6 +55,69 @@
         </v-select>
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" max-width="800px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{editedItem.title}}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-time-picker v-model="editedItem.startTime" color="green lighten-1"
+                               header-color="primary"></v-time-picker>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  prepend-icon="how_to_reg"
+                  v-model="editedItem.start" label="Начало смены"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  prepend-icon="how_to_reg"
+                  v-model="editedItem.end" label="Окончание смены"></v-text-field>
+              </v-col>
+
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="blue darken-1" text @click="close">Закрыть</v-btn>
+          <v-btn color="blue darken-1" text :disabled="false">Сохранить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogSetDuration" max-width="800px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{editedItem.title}}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <!--              <v-col cols="12" sm="6" md="4">-->
+              <v-col>
+                <v-subheader class="pl-0">Always show thumb label</v-subheader>
+                <v-slider
+                  v-model="slider"
+                  step="10"
+                  thumb-label="always"
+                ></v-slider>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="blue darken-1" text @click="close">Закрыть</v-btn>
+          <v-btn color="blue darken-1" text :disabled="false">Сохранить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -58,15 +129,36 @@
     import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
     import Employee from './index.js'
 
+    let selectId = ""
+
     export default {
         components: {
             FullCalendar // make the <FullCalendar> tag available
         },
         data: () => {
             return {
+                slider: 45,
+
+                buttons: {
+                    interval: {
+                        text: 'custom!',
+                        click: () => this.a.date.dialogSetDuration = true
+                    }
+                },
+                editedItem: {
+                    start: '',
+                    end: '',
+                    title: '',
+                    startTime: ''
+                },
+
+                dialog: false,
+                dialogSetDuration: false,
                 employeesList: [],
                 employeeSelect: "",
+                lastResource: "",
                 date: new Date().toISOString().substr(0, 10),
+                pickerDate: null,
                 minTime: "8:00",
                 maxTime: "23:00",
                 slotDuration: "00:30",
@@ -80,10 +172,10 @@
                 defaultView: 'resourceTimeGridWeek',
                 calendarWeekends: true,
                 events: function (info, successCallback, failureCallback) {
-                    console.log(info)
+                    Employee.getEvetns(info, selectId, successCallback)
                 }
-
             }
+
         },
         mounted() {
             this.initialization()
@@ -91,50 +183,82 @@
         },
 
         methods: {
-
             setResource() {
                 let calendarApi = this.$refs.fullCalendar.getApi()
-
+                console.log(this)
+                let res = calendarApi.getResourceById(this.lastResource)
+                if (res !== null) {
+                    res.remove()
+                }
                 calendarApi.addResource({
                     id: this.employeeSelect.id,
                     title: this.employeeSelect.fullName
                 })
+                this.lastResource = this.employeeSelect.id
+                selectId = this.employeeSelect.id
                 calendarApi.refetchEvents()
             },
 
             initialization() {
                 Employee.initialize(this)
+
             },
 
             toggleWeekends() {
                 this.calendarWeekends = !this.calendarWeekends // update a property
             },
-            gotoPast() {
-                let calendarApi = this.$refs.fullCalendar.getApi() // from the ref="..."
-                calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+            gotoPast(context) {
+
+
+                console.log(context)
             },
             handleDateClick(arg) {
-                //     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-                //         this.calendarEvents.push({ // add new event data
-                //             title: 'New Event',
-                //             start: arg.date,
-                //             end: (new Date + 3600)
-                //         })
-                //         console.log(this.calendarEvents)
-                //     }
             },
 
             select(arg) {
-                if (arg.view.type !== "dayGridMonth") {
-                    var json = '{ "start":"' + arg.start.getTime() + '" ,' +
-                        '"end":"' + arg.end.getTime() + '" ,' +
-                        '"resourcesId":"' + arg.resource.id + '"}';
-                    console.log("Json = " + json)
-                }
+                let calendarApi = this.$refs.fullCalendar.getApi()
+                Employee.setScheduleEmployees(arg, calendarApi)
+            },
+
+            eventClick(info) {
+                let endInfo = info.event.end;
+                let start = info.event.start.getTime() - 60000 * new Date().getTimezoneOffset();
+                let end = endInfo === null ? 0 : endInfo.getTime() - 60000 * new Date().getTimezoneOffset();
+
+                this.editedItem.start = new Date(start).toISOString().slice(0, 16)
+                this.editedItem.end = endInfo === null ? "" : new Date(end).toISOString().slice(0, 16)
+
+                this.editedItem.startTime = new Date(info.event.start.getTime())
+                this.dialog = true
+            },
+
+            eventDrop(arg) {
+                let calendarApi = this.$refs.fullCalendar.getApi()
+                Employee.eventDrop(arg, calendarApi)
+            },
+
+            eventResize(arg) {
+                let calendarApi = this.$refs.fullCalendar.getApi()
+                Employee.eventResize(arg, calendarApi)
+            },
+
+            close() {
+                this.dialog = false
             }
         },
 
-        computed: {}
+        watch: {
+            pickerDate(val) {
+                console.log(val)
+            }
+        },
+
+        computed: {
+            formTitle() {
+                return this.editedIndex === -1 ? 'Новый сотрудник' : 'Редактировать профиль'
+            },
+        }
+
     }
 </script>
 
