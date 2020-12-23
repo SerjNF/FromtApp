26b1095ba95b4c74696e9070c1d037bbbf6d3f9e
serjnf <f86n11b06>
2020-12-23 14:39:31 +0300

    edit 17.12.20

---
+++ src/components/Pages/ClientCadr/Clients.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="inspire">
+    <navigation></navigation>
+
+      <client-list v-if="$route.params.id === 0 || $route.params.id === undefined"></client-list>
+      <client-data v-if="$route.params.id >= 0" :clientId=$route.params.id></client-data>
+
+  </v-app>
+</template>
+
+<script>
+    import checkToken from "@/plugins/checkToken";
+    import store from '@/store/index'
+    import ClientList from "./clientList/ClientList"
+    import ClientData from "./clientData/ClientData"
+
+
+    export default {
+        name: 'root',
+        components: {ClientList, ClientData},
+
+        beforeRouteEnter(to, from, next) {
+
+            if (store.state.user.User.role === "PERSONAL") {
+                next(false)
+            } else {
+                checkToken.beforeRoute(to, from, next, 2)
+
+            }
+        },
+        data() {
+            return {
+                dialog: false,
+                clientId: this.$route.params.id,
+
+
+            }
+        },
+
+        methods: {
+            initialization() {
+            }
+        },
+
+        mounted() {
+            this.initialization()
+        },
+
+    }
+</script>
---
+++ src/components/Pages/ClientCadr/clientData/Orders/Invoices/Invoices.vue
@@ -0,0 +1,201 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-card
+    class="body-2">
+    <v-card-title
+      class="d-flex flex-row-reverse">
+      <span class="">Баланс: {{clId}}</span>
+    </v-card-title>
+    <v-expansion-panels>
+      <v-expansion-panel
+        v-for="i in orderData"
+        :key="i">
+        <v-expansion-panel-header>Адреса {{i.id}}
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-data-table
+            :headers="headers"
+            :items="i.orders"
+
+            item-key="id"
+            sort-by="id"
+            :items-per-page.sync="itemsPerPage"
+            :page="page"
+            hide-default-footer
+            class="elevation-0"
+          >
+            <template v-slot:top>
+              <v-btn color="blue darken-1" text right @click="dialog()">
+                <v-icon>add</v-icon>
+              </v-btn>
+            </template>
+            <template v-slot:group-by>
+
+            </template>
+            <template v-slot:footer>
+              <footerr :itemLength="orderData.length"
+                       :startItemPerPage="itemsPerPage"
+                       @changePage="changePageNumber"
+                       @changeItemPerPage="changeItemPerPag"></footerr>
+            </template>
+          </v-data-table>
+
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+
+    <v-dialog v-model="searchPrice" max-width="500px"
+              hide-overlay
+              transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-card-title>
+          <span>Добавить услугу</span>
+        </v-card-title>
+        <v-card-text>
+          <v-select
+            v-model="scheduleSelect"
+            :items="scheduleClient"
+            :item-text="selectText"
+            label="Выбрать приём"
+            prepend-icon="assignment_ind"
+            return-object>
+          </v-select>
+          <v-autocomplete
+            v-model="selectPrice"
+            :loading="loading"
+            :items="items"
+            :search-input.sync="search"
+            :item-text="priceName"
+            return-object
+            hide-no-data
+            hide-details
+            label="Код или наименование"
+            prepend-icon="search"
+          ></v-autocomplete>
+        </v-card-text>
+      </v-card>
+
+    </v-dialog>
+  </v-card>
+
+</template>
+
+<script>
+  import Footer from '@/components/Pages/Admin/Footer/Footer.vue'
+  import CurrentOrder from './index.js'
+
+  export default {
+    name: "CurrentOrder",
+    props: ["clientId"],
+    components: {
+      footerr: Footer
+    },
+    data: () => {
+      return {
+        searchPrice: false,
+        selectPrice: '',
+        items: [],
+        scheduleClient: [],
+        scheduleSelect: '',
+        page: 1,
+        itemsPerPage: 10,
+        headers: [
+          {text: '№', align: 'start', value: 'id',},
+          {text: 'Категория услуги', value: 'pricesCategory', align: 'right'},
+          {text: 'Наименование услуги', value: 'pricesName', align: 'right'},
+          {text: 'Стоимость услуги', value: 'pricesValue', align: 'right'},
+          {text: 'Количество', value: 'count', align: 'right'},
+          {text: 'Сумма', value: 'totalValue', align: 'right'},
+          {text: 'Дата', value: 'orderDate', align: 'right'},
+          {text: 'Статус', value: 'orderState', align: 'right'},
+        ],
+        orderData: [{
+          id: 1,
+          orders: [{
+            id: 1,
+            pricesCategory: "категория",
+            pricesName: "услуга",
+            pricesValue: "150",
+            count: "2",
+            totalValue: "300",
+            orderDate: "date",
+            orderState: "state"
+          }]
+        },
+          {
+            id: 2,
+            orders: [{
+              id: 2,
+              pricesCategory: "категория2",
+              pricesName: "услуга2",
+              pricesValue: "2",
+              count: "1",
+              totalValue: "200",
+              orderDate: "date1",
+              orderState: "state3"
+            }]
+          }
+
+        ],
+
+
+        clId: "",
+        loading: false,
+        search: null,
+        priceName: function (val) {
+          return val.serviceName + ' '
+        },
+
+        selectText: function (val) {
+          return "Время приема: " + val.start + ' Врач: ' + val.employee
+        }
+      }
+    },
+
+    mounted() {
+      this.clId = this.clientId;
+
+    },
+    methods: {
+      dialog() {
+        this.searchPrice = true;
+        CurrentOrder.getScheduleClientByClient(this);
+      },
+
+      querySelections(val) {
+        this.loading = true
+        //  Schedule.getClientListFiltered
+        setTimeout(() => {
+          CurrentOrder.getPriceListFiltered(val, this)
+        }, 500)
+
+        setTimeout(() => {
+          this.items = this.priceList.filter(e => {
+            return (e.serviceName || '').toLowerCase().indexOf((val || '').toLowerCase()) > -1 ||
+              (e.code || '').indexOf((val || '').toLowerCase()) > -1 ||
+              (e.id || '').indexOf((val || '').toLowerCase()) > -1
+          })
+          this.loading = false
+        }, 500)
+      },
+
+      changePageNumber(p) {
+        this.page = p
+      },
+      changeItemPerPag(i) {
+        this.itemsPerPage = i
+      },
+    },
+
+    watch: {
+      search(val) {
+        val && val !== this.select && this.querySelections(val)
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
